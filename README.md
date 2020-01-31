27a18937d71864359a1c4fa45a740a1952a79a17
aaroncouch <aaron.couch@stackpath.com>
2020-01-31 11:07:17 -0500

    Updating README and adding resources

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ StackPath's Edge Compute platform and recently open-sourced API project, Scouter
     * [Telegraf](#telegraf)
     * [InfluxDB](#influxdb)
     * [Grafana](#grafana)
-  * [Workshop Guide](#workshop_guide)
+* [Workshop Guide](#workshop_guide)
 
 <a name="getting_started"></a>
 ## Getting Started
@@ -129,8 +129,213 @@ sudo python3 -m pip install requests
 
 ### Part 2: Creating the Globally Deployed Scouter API Workload
 
+#### Creating the Workload
+
+![scouter_name_and_type](/images/scouter_name_and_type.png)
+
+![scouter_workload_settings](/images/scouter_workload_settings.png)
+
+![scouter_spec_storage_and_deployment](/images/scouter_spec_storage_and_deployment.png)
+
+![scouter_quick_overview_status](/images/scouter_quick_overview_status.png)
+
+### Modifying the Default Inbound Network Policy to Restrict Access to Grafana
+
+Let's go ahead and quickly modify the default inbound network policy that allows port 8000/TCP
+to restrict this access to our previously created VM.
+
+![scouter_default_network_policy_edit](/images/scouter_default_network_policy_edit.png)
+
+![scouter_telegraf_network_policy](/images/scouter_telegraf_network_policy.png)
+
+### From our Metric Collection VM; Validate that Scouter is Accessible and Working
+
+Proceed to your "Global Scouter API Workload" overview page and select a few of the
+public IP address of the running Scouter API instances and run the following from your
+"Metric Collection Workload" VM:
+
+**NOTE**: Please be sure to swap the following example IP address and example secret with yours.
+
+```shell
+curl -sSXGET -H "Authorization: secret" "http://151.139.47.79:8000/api/v1.0/status"
+```
+
 ### Part 3: Installing and Configuring InfluxDB on our VM
 
+Back to our VM. Let's continue there by installing and configuring InfluxDB so Telegraf has
+something to write to.
+
+#### Installation of InfluxDB v1.7.1
+
+```shell
+wget https://dl.influxdata.com/influxdb/releases/influxdb-1.7.1.x86_64.rpm
+sudo yum localinstall influxdb-1.7.1.x86_64.rpm
+sudo systemctl start influxdb
+systemctl status influxdb
+```
+
+#### Setup Database and Users/Permissions
+
+Login to Influx so we can start interacting with it:
+
+```shell
+influx
+```
+
+Once logged in let's create our database for storing performance metrics and setup users
+and permissions:
+
+```shell
+CREATE DATABASE "metricsdb";
+CREATE USER "admin" WITH PASSWORD 'changeme' WITH ALL privileges;
+CREATE USER "telegraf_wo" WITH PASSWORD 'changeme';
+CREATE USER "grafana_ro" WITH PASSWORD 'changeme';
+GRANT WRITE ON "metricsdb" TO "telegraf_wo";
+GRANT READ ON "metricsdb" TO "grafana_ro";
+exit
+```
+
+Once completed let's quickly add our admin credentials to our environment:
+
+```shell
+echo "export INFLUX_USERNAME=admin" >> ~/.bash_profile
+echo "export INFLUX_PASSWORD=[REDACTED]" >> ~/.bash_profile
+source ~/.bash_profile
+```
+
+#### Configuring HTTP Authentication
+
+InfluxDB has HTTP auth disabled by default. Let's enable that:
+
+```shell
+sudo vi /etc/influxdb/influxdb.conf
+```
+
+Locate the following section and ensure that `# auth-enabled = false` is uncommented and set to `true`:
+
+```YAML
+[http]
+  ...
+  auth-enabled = true
+```
+
+Restart the service for good measure:
+
+```shell
+sudo systemctl restart influxdb
+```
+
 ### Part 4: Installing and Configuring Telegraf on our VM
+
+Let's get the Telegraf server agent installed so we can actually start pulling peformance metrics
+from the Scouter API.
+
+#### Installation of Telegraf v1.8.3
+
+```shell
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.8.3-1.x86_64.rpm
+sudo yum localinstall telegraf-1.8.3-1.x86_64.rpm
+```
+
+#### Configuration of Performance Metric Collection
+
+Before we start the agent, let's get quickly setup to run the custom Scouter data collection script.
+
+First let's add a few environment variables to `/etc/default/telegraf` so we don't have to expose
+those in our telegraf.conf:
+
+```shell
+sudo vi /etc/default/telegraf
+```
+
+**NOTE**: Please be sure to swap out the placeholder values seen below appropriately.
+
+```shell
+SP_STACK_ID='<YOUR_STACKPATH_STACK_ID>'
+SP_API_CLIENT_ID='<YOUR_STACKPATH_API_CLIENT_ID>'
+SP_API_CLIENT_SECRET='<YOUR_STACKPATH_CLIENT_SECRET>'
+SCOUTER_WORKLOAD_ID='<YOUR_SCOUTER_WORKLOAD_ID>'
+SCOUTER_SECRET='<YOUR_SCOUTER_SECRET>'
+```
+
+For this workshop I've written a simple Python3 script that interacts with the SP2.0 API to pull
+actively running Scouter instances for the given workload and executes ICMP pings for you.
+
+[If you have a moment please review the custom script.](/resources/scouter_ping.py)
+
+Let's go ahead and copy this custom script over to `/usr/bin/` so Telegraf can access it:
+
+```shell
+sudo cp ~/developerweek_2020_workshop/resources/scouter_ping.py /usr/bin/
+```
+
+Now let's setup `/etc/telegraf/telegraf.conf` so we can actually start collecting some performance
+metrics from our Scouter API workload!
+
+Provided in this repository's resources is a preconfigured [telegraf.conf](/resources/telegraf.conf)
+file. Please take a moment to review its contents and make any appropriate changes:
+
+```shell
+vi ~/developerweek_2020_workshop/resources/telegraf.conf
+```
+
+```YAML
+[global_tags]
+[agent]
+  # Run inputs every 60 seconds.
+  interval = "60s"
+  round_interval = true
+  metric_batch_size = 1000
+  metric_buffer_limit = 10000
+  collection_jitter = "0s"
+  flush_interval = "10s"
+  flush_jitter = "0s"
+  precision = ""
+  debug = false
+  quiet = false
+  # Log output to a file.
+  logfile = "/var/log/telegraf/telegraf.log"
+  hostname = ""
+  omit_hostname = true
+
+# Configuration to output our metrics to InfluxDB.
+[[outputs.influxdb]]
+  database = "metricsdb"
+  # We created the database manually.
+  skip_database_creation = true
+  # Please be sure to modify these values if changed.
+  username = "telegraf_wo"
+  password = "changeme"
+
+# Configuration of our custom Scouter API data collection script.
+[[inputs.exec]]
+    commands = [
+      'python3 /usr/bin/scouter_ping.py --stack-id="$SP_STACK_ID" --workload-id="$SCOUTER_WORKLOAD_ID" --client-id="$SP_API_CLIENT_ID" --client-secret="$SP_API_CLIENT_SECRET" --scouter-secret="$SCOUTER_SECRET"',
+      'python3 /usr/bin/scouter_ping.py --stack-id="$SP_STACK_ID" --workload-id="$SCOUTER_WORKLOAD_ID" --client-id="$SP_API_CLIENT_ID" --client-secret="$SP_API_CLIENT_SECRET" --scouter-secret="$SCOUTER_SECRET" --dst="1.1.1.1"'
+    ]
+    timeout = "30s"
+    data_format = "influx"
+
+# Configuration of metric data type conversion.
+[[processors.converter]]
+    # A few fields returned by the custom script need to be floats.
+    [processors.converter.fields]
+      float = ["avg_rtt", "loss"]
+```
+
+Once you're done editing the configuration file; proceed with copying it to `/etc/telegraf/telegraf.conf` and finally start the server agent:
+
+```shell
+sudo cp ~/developerweek_2020_workshop/resources/telegraf.conf /etc/telegraf/telegraf.conf
+sudo systemctl enable telegraf
+sudo systemctl start telegraf
+systemctl status telegraf
+```
+
+Monitor our on-disk logging to ensure that no errors are present:
+
+```shell
+tail -f /var/log/telegraf/telegraf.log
+```
 
 ### Part 5: Installing and Configuring Grafana on our VM
---
+++ resources/scouter_ping.py
@@ -0,0 +1,187 @@
+# File: scouter_ping.py
+# Written By: Aaron Couch
+# Contributions By:
+# Date: 2020-01-29
+# Updated On: 2020-01-30
+# Python Version: 3.7.3
+# pylint: disable=locally-disabled, missing-docstring
+
+import sys
+import json
+import time
+import argparse
+import multiprocessing
+import requests
+
+SP_API_BASE_URL = "https://gateway.stackpath.com"
+
+
+class PingClient:
+    def __init__(self, client_credentials, account_data, scouter_data):
+        self._workload_instances = self._get_workload_instances(client_credentials, account_data)
+        self._scouter_secret = scouter_data[0]
+        self._scouter_port = scouter_data[1]
+        self._dst = None
+
+    @property
+    def workload_instances(self):
+        return self._workload_instances
+
+    @staticmethod
+    def _api_error_handler(message, response_json):
+        if response_json:
+            print(f"{message} Response: {json.dumps(response_json)}")
+        else:
+            print(message)
+        sys.exit(1)
+
+    @staticmethod
+    def _get_api_token(client_credentials):
+        url = f"{SP_API_BASE_URL}/identity/v1/oauth2/token"
+        payload = {"grant_type": "client_credentials"}
+        try:
+            response = requests.post(url, data=payload, auth=client_credentials)
+            response.raise_for_status()
+            data = response.json()
+            api_token = data["access_token"]
+        except requests.exceptions.RequestException as error:
+            PingClient._api_error_handler(
+                f"Failed to generate an SP2.0 API token. Error: {str(error)}", response.json()
+            )
+        return api_token
+
+    @staticmethod
+    def _get_workload_instances(client_credentials, account_data):
+        instances = {}
+        api_token = PingClient._get_api_token(client_credentials)
+        headers = {"Accept": "application/json", "Authorization": f"Bearer {api_token}"}
+        endpoint = f"/workload/v1/stacks/{account_data[0]}/workloads/{account_data[1]}/instances"
+        base_url = f"{SP_API_BASE_URL}{endpoint}"
+        has_next_page = True
+        data = None
+        while has_next_page:
+            url = base_url
+            if data is not None:
+                end_cursor = data["pageInfo"]["endCursor"]  # pylint: disable=unsubscriptable-object
+                url = f"{base_url}?page_request.after={end_cursor}"
+            try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                data = response.json()
+                for instance in data["results"]:
+                    if instance["phase"].lower() == "running":
+                        instances[instance["name"]] = {
+                            "ip_addr": instance["externalIpAddress"],
+                            "pop": instance["location"]["cityCode"].lower(),
+                        }
+                has_next_page = data["pageInfo"]["hasNextPage"]
+            except requests.exceptions.RequestException as error:
+                PingClient._api_error_handler(
+                    f"Failed to get workload instances. Error: {str(error)}", response.json()
+                )
+        return instances
+
+    def _scouter_api_handler(self, url, payload):
+        headers = {"Authorization": self._scouter_secret, "Content-Type": "application/json"}
+        try:
+            response = requests.post(url, headers=headers, data=json.dumps(payload))
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.RequestException as error:
+            if response.json():
+                return response.json()
+            return str(error)
+        receipt = data["receipt"]
+        url = f"{url}?receipt={receipt}"
+        is_running = True
+        while is_running:
+            try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                data = response.json()
+                is_running = data["is_running"]
+            except requests.exceptions.RequestException as error:
+                if response.json():
+                    return response.json()
+                return str(error)
+        return data["results"]
+
+    def _mp_ping_worker(self, instance):
+        src_ip = self._workload_instances[instance]["ip_addr"]
+        url = f"http://{src_ip}:{self._scouter_port}/api/v1.0/tests"
+        payload = {"ping": []}
+        for dst in self._dst:
+            payload["ping"].append({"dst": dst, "count": 3})
+        results = self._scouter_api_handler(url, payload)
+        return (instance, results)
+
+    def ping(self, dst, dst_pops):
+        if dst is None:
+            self._dst = []
+            for value in self._workload_instances.values():
+                if "all" in dst_pops or value["pop"] in dst_pops:
+                    self._dst.append(value["ip_addr"])
+        else:
+            self._dst = dst
+        mp_pool = multiprocessing.Pool(len(self._workload_instances.keys()))
+        results = mp_pool.map(self._mp_ping_worker, self._workload_instances)
+        mp_pool.close()
+        mp_pool.join()
+        return results
+
+def get_public_ip():
+    try:
+        response = requests.get("http://icanhazip.com")
+        response.raise_for_status()
+        return response.text.strip()
+    except requests.exceptions.RequestException:
+        return "Unknown"
+
+def get_cmd_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--stack-id", type=str, required=True)
+    parser.add_argument("--workload-id", type=str, required=True)
+    parser.add_argument("--client-id", type=str, required=True)
+    parser.add_argument("--client-secret", type=str, required=True)
+    parser.add_argument("--scouter-secret", type=str, required=True)
+    parser.add_argument("--scouter-port", type=int, default=8000)
+    dst_group = parser.add_mutually_exclusive_group()
+    dst_group.add_argument("--dst", nargs="+")
+    dst_group.add_argument("--dst-pops", nargs="+", default=["all"])
+    return vars(parser.parse_args())
+
+
+def main():
+    cmd_args = get_cmd_args()
+    public_ip = get_public_ip()
+    client = PingClient(
+        client_credentials=(cmd_args["client_id"], cmd_args["client_secret"]),
+        account_data=(cmd_args["stack_id"], cmd_args["workload_id"]),
+        scouter_data=(cmd_args["scouter_secret"], cmd_args["scouter_port"]),
+    )
+    nanoseconds = f"{time.time()*1000000000:.0f}"
+    results = client.ping(dst=cmd_args["dst"], dst_pops=cmd_args["dst_pops"])
+    for item in results:
+        src_workload_slug = item[0]
+        src_workload_pop = client.workload_instances[src_workload_slug]["pop"]
+        for result in item[1]["ping"]:
+            result = result["result"]
+            dst = result["dst"]
+            dst_pop = "Unknown"
+            avg_rtt = "9999.999"
+            if not result["failed"]:
+                avg_rtt = result["rtt"]["avg"]
+            loss = result["loss"].replace("%", "")
+            if cmd_args["dst"] is None:
+                for value in client.workload_instances.values():
+                    if value["ip_addr"] == dst:
+                        dst_pop = value["pop"]
+            print(
+                f"ping,src_slug={src_workload_slug},src_pop={src_workload_pop},dst={dst},"
+                f"dst_pop={dst_pop},public_ip={public_ip} failed=\"{int(result['failed'])}\","
+                f"loss=\"{loss}\",avg_rtt=\"{avg_rtt}\" {nanoseconds}"
+            )
+
+
+if __name__ == "__main__":
+    main()
